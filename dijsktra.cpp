b1649e74c4b7c62b60afa42bd5c1cf06c7989c56
Rohit Bajpai <83532080+Rohit-1306@users.noreply.github.com>
2021-10-14 21:21:28 +0530

    Create dijsktra.cpp

---
+++ dijsktra.cpp
@@ -0,0 +1,26 @@
+vector<int> dijsktra(vector<int>& graph[], int src) { // O((N + M) log N)
+    vector<int> d(n + 1, INT_MAX);
+    set<pair<int, int>> s; //{weight,node}
+
+    s.insert({0, src});
+    d[src] = 0;
+
+    while (s.size() != 0) {
+        auto it = s.begin();
+        int i = it -> second;
+
+        s.erase(it);
+
+        for (auto e : graph[i]) {
+            int nbr = e.first;
+            int w = e.second;
+
+            if (d[i] + w < d[nbr]) {
+                s.erase({d[nbr], nbr});
+                d[nbr] = d[i] + w;
+                s.insert({d[nbr], nbr});
+            }
+        }
+    }
+    return d;
+}
